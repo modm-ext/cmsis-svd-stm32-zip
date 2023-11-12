7b8511da1e3c54c64b91d354121836fc71fe7996
Niklas Hauser <niklas.hauser@rwth-aachen.de>
2023-11-12 22:43:15 +0100

    Setup update script

---
+++ README.md
@@ -0,0 +1,16 @@
+# Zipped CMSIS SVD files for STM32
+
+Archive of zipped SVD files for all STM32 families licensed as Apache-2.0.
+[Downloaded without modifications from the ST homepage][url].
+
+If you prefer unzipped SVD files, check out the [modm-io/cmsis-svd-stm32][repo] repo.
+
+Here is the list of the SVD version for each STM32 family:
+
+<!--families-->
+<!--/families-->
+
+This repository is updated periodically by GitHub Actions.
+
+[url]: https://www.st.com/en/microcontrollers-microprocessors/stm32-32-bit-arm-cortex-mcus.html#cad-resources
+[repo]: https://github.com/modm-io/cmsis-svd-stm32
---
+++ update.py
@@ -0,0 +1,58 @@
+import zipfile
+import logging
+import json
+import os, re, sys
+import tempfile
+import shutil
+import subprocess
+from urllib.request import urlopen, Request
+from pathlib import Path
+
+_hdr = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'
+}
+
+def download_svd_data():
+    url  = "https://www.st.com/content/st_com/en/products/microcontrollers-microprocessors/"
+    url += "stm32-32-bit-arm-cortex-mcus.cxst-rs-grid.html/CL1734.cad_models_and_symbols.svd.json"
+    with urlopen(Request(url, headers=_hdr)) as data:
+        json_data = data.read().decode(encoding="utf-8", errors="ignore")
+    rows = json.loads(json_data)["rows"]
+    return [(row["localizedDescriptions"]["en"].split(" ")[0], row["version"],
+             "https://www.st.com" + row["localizedLinks"]["en"]) for row in rows]
+
+def download_zip(url, path):
+    with tempfile.NamedTemporaryFile() as outfile:
+        os.system(f'wget -q --user-agent="{_hdr["User-Agent"]}" "{url}" -O {outfile.name}')
+        shutil.copy(outfile.name, str(path))
+
+def download_families(download = True):
+    families = []
+    for family, version, url in sorted(download_svd_data()):
+        print(family, version, url)
+        if download: download_zip(url, Path(f"{family.lower()}.zip"))
+        families.append((family, version))
+    return families
+
+def format_readme(families):
+    readme = Path("README.md")
+    readme_families = "\n".join(f"- {f}: v{v}" for f, v in families)
+    readme_families = f"<!--families-->\n{readme_families}\n<!--/families-->"
+    readme.write_text(re.sub("<!--families-->.*?<!--/families-->", readme_families,
+                             readme.read_text(), flags=re.DOTALL | re.MULTILINE))
+
+def commit_changes():
+    subprocess.run("git add README.md *.zip", shell=True)
+    if subprocess.call("git diff-index --quiet HEAD --", shell=True):
+        subprocess.run('git commit -m "Update STM32 SVD files"', shell=True)
+
+
+families = download_families("-d" in sys.argv)
+format_readme(families)
+if "-c" in sys.argv:
+    commit_changes()
